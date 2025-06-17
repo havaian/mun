d5339e4c1f7904da10bf2de023b6de5b1ad66dbc
Malik <m.usman.work@gmail.com>
2025-06-17 15:09:58 +0500

    Updated App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="min-h-screen" :class="{ 'gradient-bg': isHomePage }">
-    <AppHeader v-if="!isHomePage" />
-    <main :class="isHomePage ? '' : 'container mx-auto py-6 px-4 sm:px-6 lg:px-8'">
+  <div class="min-h-screen bg-gray-50 gradient-bg">
+    <AppHeader />
+    <main class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
       <router-view />
     </main>
-    <AppFooter v-if="!isHomePage" />
+    <AppFooter />
     <Vue3Toastify position="bottom-right" />
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
 import Vue3Toastify from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
 import AppHeader from './components/AppHeader.vue'
 import AppFooter from './components/AppFooter.vue'
-
-const route = useRoute()
-
-const isHomePage = computed(() => route.name === 'home')
 </script>
-</template>
