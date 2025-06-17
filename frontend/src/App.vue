438b73d534348c45fd84e1592341d59191414afb
Malik <m.usman.work@gmail.com>
2025-06-17 13:31:15 +0500

    Recreate Coming Soon Page Design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="min-h-screen bg-gray-50">
-    <AppHeader />
-    <main class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
+  <div class="min-h-screen">
+    <AppHeader v-if="!isHomePage" />
+    <main :class="isHomePage ? '' : 'container mx-auto py-6 px-4 sm:px-6 lg:px-8'">
       <router-view />
     </main>
-    <AppFooter />
+    <AppFooter v-if="!isHomePage" />
     <Vue3Toastify position="bottom-right" />
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import Vue3Toastify from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
 import AppHeader from './components/AppHeader.vue'
 import AppFooter from './components/AppFooter.vue'
+
+const route = useRoute()
+
+const isHomePage = computed(() => route.name === 'home')
 </script>
+</template>
