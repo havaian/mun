ea9f568e5454d3253c4711a6be07f49923a07cdd
havaian <m.usman.work@gmail.com>
2025-09-04 17:47:28 +0500

    Project reset

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div class="min-h-screen bg-gray-50 gradient-bg">
-    <AppHeader />
-    <main class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <router-view />
-    </main>
-    <AppFooter />
-    <Vue3Toastify position="bottom-right" />
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script setup>
-import Vue3Toastify from 'vue3-toastify'
-import 'vue3-toastify/dist/index.css'
-import AppHeader from './components/AppHeader.vue'
-import AppFooter from './components/AppFooter.vue'
-</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss
-  ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  }
+  plugins: [vue()],
 })
