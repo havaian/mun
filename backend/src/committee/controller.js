2f7b3c5371f057fbf276f156b385cf62cbbc5a83
havaian <m.usman.work@gmail.com>
2026-02-21 01:03:11 +0500

    Updated backend for committee creating again

--- backend/src/committee/controller.js
+++ backend/src/committee/controller.js
@@ -13,6 +13,7 @@ const createCommittee = async (req, res) => {
         const {
             eventId,
             name,
+            acronym,
             type,
             description,
             language = 'english'
